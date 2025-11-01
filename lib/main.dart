52c3e6877f16b7eba7092ac88b094d2d541b90e4
Antho-321 <allunav@utn.edu.ec>
2025-11-01 10:44:38 -0500

    actualizo para pruebas en cas

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ Future<void> main() async {
     // ip casa: 192.168.100.7
     // ip DDTI: 172.16.14.238
     // EDUROAM: 172.20.152.53
-    const offerUrl = 'http://172.20.152.53:8000/webrtc/offer';
+    const offerUrl = 'http://192.168.100.7:8000/webrtc/offer';
 
     // 1) Registrar dependencias (pasa la config del servicio aquí)
     registrarDependenciasPosture(
